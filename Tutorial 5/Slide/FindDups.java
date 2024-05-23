690a912787bfd8ce3f9c5419d6ee93cc3c2c761e
Erdianti Wiga Putri Andini <13522053@mahasiswa.itb.ac.id>
2024-05-23 11:37:02 +0700

    feat: remove unused

--- Tutorial 5/Slide/FindDups.java
+++
@@ -1,9 +0,0 @@
-import java.util.*;
-public class FindDups {
- public static void main(String[] args) {
- Set<String> s = new HashSet<String>();
- for (String a : args) 
- s.add(a);
- System.out.println(s.size() + " distinct words: " + s);
- }
-}
